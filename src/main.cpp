066b80bac95e32ab3d9da4d2811725d86880c681
harshit-002 <harshitjoshi3238@gmail.com>
2024-08-28 18:46:54 +0530

    step 5

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-
   while(true){
     std::cout << "$ ";
     std::string input;
